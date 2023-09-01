24846f3c5e06c83be0a2f9b8126063ec5b8c2a94
João Martins Filho <joaomfilho1995@gmail.com>
2023-09-01 14:41:56 -0300

    adicionado xk6-dashboard para testes

--- scripts/script2.js
+++ scripts/script2.js
@@ -4,9 +4,9 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 10 },
-    { duration: '20s', target: 0 },
+    { duration: '1m', target: 10 },
+    { duration: '1m', target: 20 },
+    { duration: '2m', target: 30 },
   ],
 };
 
