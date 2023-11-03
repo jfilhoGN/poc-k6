d8e27825c21e92789c2a995cbb710ca566a39e3c
João Martins Filho <joaomfilho1995@gmail.com>
2023-11-02 22:59:00 -0300

    criando função para pegar item sequencial

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,16 +1,32 @@
 // example rampage
+//https://k6.io/docs/using-k6/execution-context-variables/#__vu-and-__iter-discouraged
+//https://k6.io/docs/examples/data-parameterization/#retrieving-unique-data
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import {exec, scenario} from 'k6/execution';
+
+export function randomItem(arrayOfItems) {
+  return arrayOfItems[Math.floor(Math.random() * arrayOfItems.length)];
+}
 
 export const options = {
   stages: [
-    { duration: '1m', target: 10 },
-    { duration: '1m', target: 20 },
-    { duration: '2m', target: 30 },
+    { duration: '1m', target: 3 }
   ],
 };
 
+function getItem(arrayOfItems, iteration) {
+  return arrayOfItems[iteration]
+}
+
 export default function () {
+  const array = []
+  const size = 2000
+  for (let i = 0; i < size; i++) {
+    array.push(i)
+  }
+  let id = getItem(array, `${scenario.iterationInTest}`)
+  console.log(id)
   const res = http.get('https://httpbin.test.k6.io/');
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
